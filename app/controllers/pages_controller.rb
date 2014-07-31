53246e67f3a7d883d895e07c87bb9bafc613ce0d
pintergreg <pinter.gergo@vipmail.hu>
2014-07-31 17:37:16 +0200

    Initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+    def home 
+    end
+    
+    def result
+        require 'inifile'
+        $inifile = IniFile.load('lib/data.ini')
+        if $inifile.nil? then
+            puts "GORRAM"
+        end
+        require 'enigma'
+        e=EnigmaCipher.new(params[:rotI].to_i, params[:rotII].to_i, params[:rotIII].to_i, params[:RIoff], params[:RIIoff], params[:RIIIoff], params[:ref].to_i)
+        @result=e.encodeString(params[:input])
+    end
+end
